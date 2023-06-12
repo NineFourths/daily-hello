1ad7f4c9036462603fc0a6b8d5290bb9c9e65ece
kk_fun <2450690281@qq.com>
2023-06-12 18:53:49 +0800

    新增了长文本切割成短文本相连的功能

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ app_secret = os.environ["APP_SECRET"]
 user_ids = os.environ["USER_ID"].split("\n")
 template_id = os.environ["TEMPLATE_ID"]
 
+r1 = []
+
 
 def get_weather():
   url = "https://devapi.qweather.com/v7/weather/3d?lang=cn&gzip=n&location=101280102&key=300db3a6b17447f7bbf92aca06a6aee3"
@@ -45,10 +47,15 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+  # words = requests.get("https://api.shadiao.pro/chp")
+  # if words.status_code != 200:
+  #   return get_words()
+  # return words.json()['data']['text']
+
+  url = "https://devapi.qweather.com/v7/weather/now?lang=cn&gzip=n&location=cn101280102&key=300db3a6b17447f7bbf92aca06a6aee3"
+  res = requests.get(url).json()
+  text = res['now']['text']
+  return text
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
@@ -70,6 +77,21 @@ def get_wea_war():
   else:
     return "要是下雨记得带伞捏"
 
+def slice_love_words():
+    s1 = get_words()
+    maxLen = 19
+    maxList = 6
+    for i in range(len(s1)):
+        if i % maxLen == 0 and i != 0:
+            r1.append(s1[i - maxLen: i])
+    t = len(s1) % maxLen
+    r1.append(s1[len(s1) - t:])
+    if len(r1) < maxList:
+        for i in range(maxList - len(r1)):
+            r1.append('')
+
+def get_slice_words(cnt):
+    return r1[cnt]
 
 
 client = WeChatClient(app_id, app_secret)
@@ -79,6 +101,7 @@ wea, temperature, highest, lowest = get_weather()
 now_year = today.year
 now_month = today.month
 now_day = today.day
+slice_love_words()
 data = {"weather_warning":{"value": get_wea_war(), "color":get_random_color()},
         "date_D":{"value":now_day, "color":get_random_color()},
         "date_M":{"value":now_month, "color":get_random_color()},
@@ -92,6 +115,12 @@ data = {"weather_warning":{"value": get_wea_war(), "color":get_random_color()},
         "highest": {"value":highest,"color":get_random_color()},
         "lowest":{"value":lowest, "color":get_random_color()},
         "city":{"value":city, "color":get_random_color()},
+        "words0":{"value":get_slice_words(0),"color":get_random_color()},
+        "words1":{"value":get_slice_words(1),"color":get_random_color()},
+        "words2":{"value":get_slice_words(2),"color":get_random_color()},
+        "words3":{"value":get_slice_words(3),"color":get_random_color()},
+        "words4":{"value":get_slice_words(4),"color":get_random_color()},
+        "words5":{"value":get_slice_words(5),"color":get_random_color()},
         }
 
 count = 0
